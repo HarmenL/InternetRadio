f072399b5b535b0e40d6dd133d29158f112d77bc
MalekSediqi <maleksediqi17@gmail.com>
2016-03-23 21:11:34 +0100

    buttonprefix.c en .h

---
+++ buttonPrefix.c
@@ -0,0 +1,14 @@
+//
+// Created by malek on 23-3-2016.
+//
+
+#include "keyboard.h"
+
+void buttonCheck()
+{
+    int buttons[5]  = {};
+
+    for (int i = 0; i < 5; ++i) {
+
+    }
+}
---
+++ buttonPrefix.h
@@ -0,0 +1,17 @@
+//
+// Created by malek on 23-3-2016.
+//
+
+#ifndef INTERNETRADIO_BUTTONPREFIX_H
+#define INTERNETRADIO_BUTTONPREFIX_H
+
+#endif //INTERNETRADIO_BUTTONPREFIX_H
+struct _LeftButtons
+{
+    u_short id;
+    u_short bid;
+    char name[16];
+    char ip[24];
+    u_short port;
+    char url[24];
+};
